010d4125ecf93219c8d91f42f4799906f59f615d
Michael Mosmann <michael@mosmann.de>
2020-03-19 06:58:27 +0100

    can show qr-code

--- app/src/main/java/de/flapdoodle/fightcorona/RegisterUserFragment.kt
+++ app/src/main/java/de/flapdoodle/fightcorona/RegisterUserFragment.kt
@@ -1,12 +1,16 @@
 package de.flapdoodle.fightcorona
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.ImageView
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.google.zxing.BarcodeFormat
+import com.journeyapps.barcodescanner.BarcodeEncoder
+
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
@@ -26,6 +30,14 @@ class RegisterUserFragment : Fragment() {
 
         view.findViewById<Button>(R.id.button_second).setOnClickListener {
             findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
+        }
+        try {
+            val barcodeEncoder = BarcodeEncoder()
+            val bitmap =
+                barcodeEncoder.encodeBitmap("content", BarcodeFormat.QR_CODE, 400, 400)
+            val imageViewQrCode = view.findViewById<ImageView>(R.id.qr_code)
+            imageViewQrCode.setImageBitmap(bitmap)
+        } catch (e: Exception) {
         }
     }
 }
